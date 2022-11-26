eba5b4babfa7f73f24cd4736e3b4671b068c8204
Gian <epilred@icloud.com>
2022-11-26 09:05:56 +0800

    added project infographic and document

--- components/nav.js
+++ components/nav.js
@@ -19,6 +19,23 @@ const Nav = ({ theme, theme_callback, toggle_callback }) => {
 	return (
 		<nav className={nav_styles.navbar}>
 			<ul className={nav_styles.navbar_nav}>
+				<li className={nav_styles.logo}>
+					<Link href="/"><a className={nav_styles.nav_link}>
+						<span className={nav_styles.link_text, nav_styles.logo_text}>AAPP5</span>
+						<svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="angle-double-right"
+							role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+							className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x">
+							<g className={nav_styles}>
+								<path fill="currentColor"
+									d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+									className={nav_styles.fa_secondary}></path>
+								<path fill="currentColor"
+									d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+									className={nav_styles.fa_primary}></path>
+							</g>
+						</svg></a>
+					</Link>
+				</li>
 				<li className={nav_styles.nav_item}>
 					<Link href="/"><a className={nav_styles.nav_link}>
 						<HomeIcon/>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,20 @@
 import Image from "next/image";
 import Head from 'next/head';
-import Link from 'next/link';
 
 import React, { useState, useEffect } from "react";
 
 import debug_ from '../components/debug_helper';
 
-import inner_nav from '../styles/InnerNav.module.css';
+// import Aapp5Svg from "../public/svg/aapp5-infographic.svg";
 
-import FunctionIcon from '../public/svg/function-solid.svg';
-import EqualsIcon from '../public/svg/equals-solid.svg';
-import LessThanEqualIcon from '../public/svg/less-than-equal-solid.svg';
-
-export default function Home({m_posts}) {
+export default function Home() {
 
 	return (
 		<>
 			<Head>
 				<title>Home</title>
 			</Head>
+			{/* <Aapp5Svg/> */}
 		</>	
-	)
+	);
 }
